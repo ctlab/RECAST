8d8cd59b0e4a25c6a7682aac656f2ac73f442ea6
ivartb <ivart07@mail.ru>
2019-08-23 18:05:56 +0300

    Add results visualisation for simple reads classifier

---
+++ fmt_visualiser.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+o="outDir"
+w="workDir"
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -k|--k)
+    k="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -d|--donor-files)
+    d1="$2"
+    d2="$3"
+    shift # past argument
+    shift # past value
+    shift # past value
+    ;;
+    -b|--before-files)
+    b1="$2"
+    b2="$3"
+    shift # past argument
+    shift # past value
+    shift # past value
+    ;;
+    -a|--after-files)
+    a1="$2"
+    a2="$3"
+    shift # past argument
+    shift # past value
+    shift # past value
+    ;;
+    -o|--output-dir)
+    o="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -w|--work-dir)
+    w="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -i|--input-dir)
+    i="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -m|--memory)
+    m="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--available-processors)
+    p="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -v|--verbose)
+    v=true
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+cmd="java "
+if [[ $m ]]; then
+    cmd+="-Xmx${m} -Xms${m} "
+fi
+cmd+="-jar metacherchant.jar -t fmt-visualiser "
+if [[ $k ]]; then
+    cmd+="-k $k "
+fi
+if [[ $m ]]; then
+    cmd+="-m $m "
+fi
+if [[ $p ]]; then
+    cmd+="-p $p "
+fi
+if [[ $v ]]; then
+    cmd+="-v "
+fi
+cmd+="-w $w -o $o"
+if [[ ${d1} ]]; then
+    cmd+="-donor ${d1} ${d2} "
+fi
+if [[ ${b1} ]]; then
+    cmd+="-before ${b1} ${b2} "
+fi
+if [[ ${a1} ]]; then
+    cmd+="-after ${a1} ${a2} "
+fi
+if [[ $i ]]; then
+    cmd+="-i $i "
+fi
+
+echo "$cmd"
+$cmd
+if [[ $? -eq 0 ]]; then
+    echo "SUCCESS"
+else
+    echo "error while after reads processing"
+    exit -1
+fi
